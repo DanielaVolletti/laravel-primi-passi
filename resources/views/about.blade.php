55a4314c4af789852e507b55501f5562a7ee3622
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-06-09 20:13:08 +0200

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,24 @@
 @extends('mainLayout')
 
 @section('content')
-  <h1>L'emblema</h1>
-  <p>L'emblema dello struzzo che contraddistingue le edizioni Einaudi è stato ereditato dalla rivista «La Cultura» di cui Giulio Einaudi fu l'ultimo editore, prima che il regime fascista la sopprimesse nel 1935. Ma l'origine del logo risale a prima del 1574, anno in cui fu pubblicato nel volume Dialogo delle imprese militari et amorose di Monsignor Paolo Giovio. Il libro è una raccolta di simboli e motivi allegorici (le "imprese"), che Giovio stesso ideava su commissione per i Signori e i Capitani che li facevano ricamare sulle bandiere e sulle livree delle proprie Compagnie.
-  L'immagine dello struzzo è stata ideata da Giovio per "l'impresa" di Girolamo Mattei Romano «capitan de' cavalli della guardia di Papa Clemente, huomo risoluto e d'alto pensiero, e d'animo deliberato». Mattei, che aveva aspettato con pazienza e perseveranza il momento per vendicare l'uccisione del fratello, aveva chiesto a Monsignor Giovio di ideargli un emblema «significante ch'un valoroso cuore hà forza di smaltire ogni grave ingiuria», è questo il senso originario del motto «Spiritus durissima coquit» che accompagna l'immagine dello struzzo.
-  </p>
-  <h3>Pablo Picasso</h3>
-  <p>La collana Einaudi Tascabili ha come logo uno struzzo che corre, privo del motto. Si tratta di un disegno realizzato da Picasso che ne fece dono a Giulio Einaudi nel 1951, in occasione di una visita dell'editore nella sua residenza di Antibes. In quel periodo Picasso stava completando l'illustrazioni per un'edizione francese delle Storie naturali di Georges-Louis Leclerc de Buffon. Tra le immagini figurava anche questo struzzo che piacque moltissimo a Einaudi e che Picasso volle regalargli nella sua versione originale.</p>
-  <h3>Giacomo Manzù</h3>
-  <p>Del logo originario esiste una diversa e raffinata versione disegnata da Giacomo Manzù nel 1961, in occasione della pubblicazione per la Giulio Einaudi editore di una raccolta di sue riproduzioni in edizione limitata: i Quarantun disegni di Giacomo Manzù.</p>
-  <h3>Giulio Paolini</h3>
-  <p>Nell'ottobre 2000, in occasione della fiera di Francoforte, è stato presentato un nuovo struzzo d'artista realizzato da Giulio Paolini. La bellissima interpretazione di Paolini del marchio Einaudi mostra uno struzzo stilizzato contenente quello originario, come ad indicare che l'innovazione della casa editrice conserva comunque una continuità con la propria tradizione.</p>
-
+  <div class="casa-editrice">
+    <h1>La casa editrice</h1>
+    <div class="box-casa-ed">
+      <h2>L'emblema</h2>
+      <p>L'emblema dello struzzo che contraddistingue le edizioni Einaudi è stato ereditato dalla rivista «La Cultura» di cui Giulio Einaudi fu l'ultimo editore, prima che il regime fascista la sopprimesse nel 1935. Ma l'origine del logo risale a prima del 1574, anno in cui fu pubblicato nel volume Dialogo delle imprese militari et amorose di Monsignor Paolo Giovio. Il libro è una raccolta di simboli e motivi allegorici (le "imprese"), che Giovio stesso ideava su commissione per i Signori e i Capitani che li facevano ricamare sulle bandiere e sulle livree delle proprie Compagnie.
+      L'immagine dello struzzo è stata ideata da Giovio per "l'impresa" di Girolamo Mattei Romano «capitan de' cavalli della guardia di Papa Clemente, huomo risoluto e d'alto pensiero, e d'animo deliberato». Mattei, che aveva aspettato con pazienza e perseveranza il momento per vendicare l'uccisione del fratello, aveva chiesto a Monsignor Giovio di ideargli un emblema «significante ch'un valoroso cuore hà forza di smaltire ogni grave ingiuria», è questo il senso originario del motto «Spiritus durissima coquit» che accompagna l'immagine dello struzzo.
+      </p>
+    </div>
+    <div class="box-casa-ed">
+      <h3>Pablo Picasso</h3>
+      <p>La collana Einaudi Tascabili ha come logo uno struzzo che corre, privo del motto. Si tratta di un disegno realizzato da Picasso che ne fece dono a Giulio Einaudi nel 1951, in occasione di una visita dell'editore nella sua residenza di Antibes. In quel periodo Picasso stava completando l'illustrazioni per un'edizione francese delle Storie naturali di Georges-Louis Leclerc de Buffon. Tra le immagini figurava anche questo struzzo che piacque moltissimo a Einaudi e che Picasso volle regalargli nella sua versione originale.</p>
+      <h3>Giacomo Manzù</h3>
+      <p>Del logo originario esiste una diversa e raffinata versione disegnata da Giacomo Manzù nel 1961, in occasione della pubblicazione per la Giulio Einaudi editore di una raccolta di sue riproduzioni in edizione limitata: i Quarantun disegni di Giacomo Manzù.</p>
+    </div>
+    <div class="box-casa-ed">
+      <h3>Giulio Paolini</h3>
+      <p>Nell'ottobre 2000, in occasione della fiera di Francoforte, è stato presentato un nuovo struzzo d'artista realizzato da Giulio Paolini. La bellissima interpretazione di Paolini del marchio Einaudi mostra uno struzzo stilizzato contenente quello originario, come ad indicare che l'innovazione della casa editrice conserva comunque una continuità con la propria tradizione.</p>
+    </div>
+  </div>
 
 @endsection
