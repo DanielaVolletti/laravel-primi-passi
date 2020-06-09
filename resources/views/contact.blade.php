053cfe30a2882ea6b5a09719e5551a0426480aa7
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-06-09 17:47:10 +0200

    conclusione struttura grezza

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,45 @@
 @extends('mainLayout')
 
 @section('content')
-  <p>questa è la sezione contatti</p>
+  <h1>Contatti</h1>
+  <p>Chi desidera ricevere informazioni cliccando direttamente gli indirizzi e-mail indicati, o partecipare a blog e forum, fornirà spontaneamente il suo indirizzo e-mail e/o altri dati alla società Giulio Einaudi Editore Spa.
+
+  Per informazioni sulla privacy clicca <a href="#">qui.</a></p>
+
+  <div class="domande">
+    <h2>Risposte alle domande più frequenti</h2>
+    <div class="domande-risposte">
+      <h3>Come devo fare per spedirvi una proposta editoriale o un manoscritto?</h3>
+      <p>È possibile inviare i testi, preferibilmente in formato Word o Pdf:
+      <ul>
+        <li>1) per la narrativa a: einaudiproposte@einaudi.it. Verranno prese in considerazione solo opere complete, eventualmente accompagnate da una breve descrizione: non saranno valutati incipit, sinossi o scalette;</li>
+        <li>2) per la saggistica e la poesia a: einaudi@einaudi.it allegando un curriculum vitae.
+        Si prega di inserire, oltre che nel corpo della mail, anche nel documento allegato i propri dati anagrafici comprensivi di indirizzo di posta elettronica e numero telefonico.</li>
+      </ul>
+      Tutti i testi vengono presi in considerazione. Riceveranno risposta (in circa sei mesi) solo quelli che, a giudizio della redazione, risulteranno degni di interesse. In ogni caso i dattiloscritti cartacei non verranno restituiti.</p>
+    </div>
+    <div class="domande-risposte">
+      <h3>Posso contattare personalmente un Autore della casa editrice?</h3>
+      <p>Le comunicazioni dirette ai nostri Autori vanno indirizzate, a loro nome,
+      c/o Giulio Einaudi editore
+      Att.ne Ufficio comunicazione
+      Via Biancamano, 2
+      10121 Torino</p>
+    </div>
+    <div class="domande-risposte">
+      <h3>Come devo fare per inviare il mio Curriculum?</h3>
+      <p>Einaudi non accetta CV inviati per posta elettronica; si consiglia di inviarli a:
+      Giulio Einaudi editore
+      Via Biancamano, 2
+      10121 Torino.</p>
+    </div>
+
+    <div class="domande-risposte">
+      <h3>Posso trovare presso di voi un libro Einaudi fuori catalogo?</h3>
+      <p>Einaudi non possiede copie di libri fuori catalogo da vendere al pubblico.</p>
+    </div>
+
+  </div>
+
+
 @endsection
